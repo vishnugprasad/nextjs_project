c84e98f0d4c67e76e9d435f5467c13add2cd03ef
vishnugprasad <vishnuanugraha001@gmail.com>
2024-01-29 14:35:30 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
 
+import {Hero } from '@/components'
+import {CustomButton } from '@/components'
+
+
+
 export default function Home() {
   return (
     <main className="overflow-hidden">
-     
+     <Hero/>
+     <CustomButton/>
     </main>
   )
 }
